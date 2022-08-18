a7f01d3dd964955ac54c313fba6243e2b6b306cd
JuskingA <juskingadjeinyarko@gmail.com>
2022-08-18 20:56:25 +0100

    include fixed

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include "holberton.h"
 
 /**
  * _pow - calculates (base ^ power)
--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include "holberton.h"
 
 /**
  * flip_bits - number of different bits between two numbers
